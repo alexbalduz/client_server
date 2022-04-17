9bf133c3de6fc7a5786e08a18d814866a0174fdb
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2022-04-17 12:58:50 +0200

    prints no funcionan

--- server.py
+++ server.py
@@ -11,30 +11,30 @@ server_address = ('localhost', 10000)
 #Aceptamos conexiones entrantes con el metodo listen. Por parámetro las conexiones simutáneas.
 socket.bind(server_address)
 
-print >>sys.stderr, 'empezando a levantar %s puerto %s' % server_address
+#print >>sys.stderr, 'empezando a levantar %s puerto %s' % server_address
 
 # poner socket a modo de servidor
 socket.listen(1)
 
 while True:
     # Esperando conexion
-    print >>sys.stderr, 'Esperando para conectarse'
+    #print >>sys.stderr, 'Esperando para conectarse'
 
     #accept, acepta una conexión
     connection, client_address = socket.accept()
 
     try:
-        print >>sys.stderr, 'concexion desde', client_address
+        #print >>sys.stderr, 'concexion desde', client_address
 
         # Recibe los datos en trozos y reetransmite
         while True:
             data = connection.recv(19)
-            print >>sys.stderr, 'recibido "%s"' % data
+            #print >>sys.stderr, 'recibido "%s"' % data
             if data:
-                print >>sys.stderr, 'enviando mensaje de vuelta al cliente'
+                #print >>sys.stderr, 'enviando mensaje de vuelta al cliente'
                 connection.sendall(data)
             else:
-                print >>sys.stderr, 'no hay mas datos', client_address
+                #print >>sys.stderr, 'no hay mas datos', client_address
                 break
 
     finally:
