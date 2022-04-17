694780d90ea5a159d03b7ccafebc45a0c2ffa1d0
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2022-04-17 13:25:27 +0200

    funcion service

--- multiconex_server.py
+++ multiconex_server.py
@@ -3,6 +3,24 @@ import socket
 import selectors
 import types
 
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)  # Should be ready to read
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print(f"Closing connection to {data.addr}")
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print(f"Echoing {data.outb!r} to {data.addr}")
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
+
+
 def accept_wrapper(sock):
     #para poner el socket en un modo de no-bloqueo
     conn, addr = sock.accept()
